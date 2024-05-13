42bae5a0fbab34781b6a429f8ad7d67ec6f2a207
Bernardogattiamaral <gattibermito@gmail.com>
2024-05-13 14:04:59 -0300

    contador de vendas

---
+++ Contador de Vendas/script.js
@@ -0,0 +1,18 @@
+const product_section = document.querySelector(".product-form")
+var form = document.querySelector(".form")
+function cadastrarproduto() {
+    var product_n = document.getElementById("productname")
+    var product_p1 = document.getElementById("productprice1")
+    var product_p2 = document.getElementById("productprice2")
+    var product_total = document.getElementById("producttotal")
+    var product1 = document.getElementById("product1")
+    var product2 = document.getElementById("product2")
+    var product3 = document.getElementById("product3")
+    var product4 = document.getElementById("product4")
+    var product5 = document.getElementById("product5")
+    var product6 = document.getElementById("product6")
+    var product7 = document.getElementById("product7")
+    var product8 = document.getElementById("product8")
+
+    form = ''
+}
